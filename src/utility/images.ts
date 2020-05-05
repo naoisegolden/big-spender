5b38f15c9e004467bdb3a50faeb9df3238a5523c
chrisbmar <50221462+chrisbmar@users.noreply.github.com>
2020-05-05 11:14:09 +0200

    add images for all items

--- src/utility/images.ts
+++ src/utility/images.ts
@@ -1,9 +1,53 @@
 import beachHouse from "../assets/itemImages/beachHouse.jpg";
 import airplane from "../assets/itemImages/privatejet.jpg";
+import antilia from "../assets/itemImages/antilia.jpg";
+import barcelona from "../assets/itemImages/barcelona.jpg";
+import birkinbag from "../assets/itemImages/birkinbag.jpg";
+import boobjob from "../assets/itemImages/boobjob.jpg";
+import campervan from "../assets/itemImages/campervan.jpg";
+import car from "../assets/itemImages/car.jpg";
+import citizenship from "../assets/itemImages/citizenship.jpg";
+import coat from "../assets/itemImages/coat.jpg";
+import cosmetics from "../assets/itemImages/cosmetics.jpg";
+import f1car from "../assets/itemImages/f1car.jpg";
+import fries from "../assets/itemImages/fries.jpg";
+import island from "../assets/itemImages/island.jpg";
+import lakers from "../assets/itemImages/lakers.jpg";
+import madrid from "../assets/itemImages/madrid.jpg";
+import manutd from "../assets/itemImages/manutd.jpg";
+import netflix from "../assets/itemImages/netflix.jpg";
+import plant from "../assets/itemImages/plant.jpg";
+import playstation from "../assets/itemImages/playstation.jpg";
+import washingtonpost from "../assets/itemImages/washingtonpost.jpg";
+import yeezy from "../assets/itemImages/yeezy.jpg";
+import ticket from "../assets/itemImages/ticket.jpg";
+import yacht from "../assets/itemImages/yacht.jpg";
 
 const images = {
   beachHouse,
   airplane,
+  antilia,
+  barcelona,
+  birkinbag,
+  boobjob,
+  campervan,
+  car,
+  citizenship,
+  coat,
+  cosmetics,
+  f1car,
+  fries,
+  island,
+  lakers,
+  manutd,
+  madrid,
+  netflix,
+  plant,
+  yeezy,
+  playstation,
+  washingtonpost,
+  ticket,
+  yacht,
 };
 
 export default images;
